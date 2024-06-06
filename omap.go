0a4fcee0b7a0cb530c7d907f0982d7d4aee2b119
Russ Cox <rsc@golang.org>
2024-06-06 14:29:02 -0400

    omap: add DeleteRange, better tests

--- omap.go
+++ omap.go
@@ -20,15 +20,26 @@ import (
 // A Map is a map[K]V ordered according to K's standard Go ordering.
 // The zero value of a Map is an empty Map ready to use.
 type Map[K cmp.Ordered, V any] struct {
-	root *node[K, V]
+	_root *node[K, V]
 }
 
 // A MapFunc is a map[K]V ordered according to an arbitrary comparison function.
 // The zero value of a MapFunc is not meaningful since it has no comparison function.
 // Use [NewMapFunc] to create a [MapFunc].
+// A nil *MapFunc, like a nil Go map, can be read but not written and contains no entries.
 type MapFunc[K, V any] struct {
-	root *node[K, V]
-	cmp  func(K, K) int
+	_root *node[K, V]
+	cmp   func(K, K) int
+}
+
+// A node is a node in the treap.
+type node[K any, V any] struct {
+	parent *node[K, V]
+	left   *node[K, V]
+	right  *node[K, V]
+	key    K
+	val    V
+	pri    uint64
 }
 
 // NewMapFunc returns a new MapFunc[K, V] ordered according to cmp.
@@ -36,39 +47,48 @@ func NewMapFunc[K, V any](cmp func(K, K) int) *MapFunc[K, V] {
 	return &MapFunc[K, V]{cmp: cmp}
 }
 
-// A node is a node in the treap.
-type node[K any, V any] struct {
-	parent *node[K, V]
-	prev   *node[K, V]
-	next   *node[K, V]
-	key    K
-	val    V
-	pri    uint64
+// omap is the interface implemented by both Map[K, V] and MapFunc[K, V]
+// that enables a common implementation of the map operations.
+type omap[K, V any] interface {
+	// root returns &m._root; the caller can read or write *m.root().
+	root() **node[K, V]
+
+	// find reports where a node with the key would be: at *pos.
+	// If *pos != nil, then key is present in the tree;
+	// otherwise *pos is where a new node with the key should be attached.
+	//
+	// If parent != nil, then pos is either &parent.left or &parent.right
+	// depending on how parent.key compares with key.
+	// If parent == nil, then pos is m.root().
+	find(key K) (pos **node[K, V], parent *node[K, V])
 }
 
-// lookup looks up the key k in the map.
+func (m *Map[K, V]) root() **node[K, V]     { return &m._root }
+func (m *MapFunc[K, V]) root() **node[K, V] { return &m._root }
+
+// find looks up the key k in the map.
 // It returns the parent of k as well as the position where k would be attached.
 // *pos is non-nil if k is present, nil if k is missing.
 // parent is nil if there are no nodes in the map, or if there is only one node and it's k.
-func (m *Map[K, V]) lookup(k K) (parent *node[K, V], pos **node[K, V]) {
-	pos = &m.root
+func (m *Map[K, V]) find(k K) (pos **node[K, V], parent *node[K, V]) {
+	pos = &m._root
 	for x := *pos; x != nil; x = *pos {
 		if x.key == k {
 			break
 		}
 		parent = x
 		if x.key > k {
-			pos = &x.prev
+			pos = &x.left
 		} else {
-			pos = &x.next
+			pos = &x.right
 		}
 	}
-	return parent, pos
+	return pos, parent
 }
 
-// lookup is the same as for Map[K, V] but using m.cmp.
-func (m *MapFunc[K, V]) lookup(k K) (parent *node[K, V], pos **node[K, V]) {
-	pos = &m.root
+// find is the same as for Map[K, V] but using m.cmp.
+func (m *MapFunc[K, V]) find(k K) (pos **node[K, V], parent *node[K, V]) {
+	pos = &m._root
 	for x := *pos; x != nil; x = *pos {
 		cmp := m.cmp(x.key, k)
 		if cmp == 0 {
@@ -76,132 +96,182 @@ func (m *MapFunc[K, V]) lookup(k K) (parent *node[K, V], pos **node[K, V]) {
 		}
 		parent = x
 		if cmp > 0 {
-			pos = &x.prev
+			pos = &x.left
 		} else {
-			pos = &x.next
+			pos = &x.right
 		}
 	}
-	return parent, pos
+	return pos, parent
+}
+
+// Get returns the value of m[key] and reports whether it exists.
+func (m *Map[K, V]) Get(key K) (V, bool) {
+	return get(m, key)
+}
+
+// Get returns the value of m[key] and reports whether it exists.
+func (m *MapFunc[K, V]) Get(key K) (V, bool) {
+	return get(m, key)
+}
+
+func get[K, V any](m omap[K, V], key K) (V, bool) {
+	pos, _ := m.find(key)
+	if x := *pos; x != nil {
+		return x.val, true
+	}
+	var zero V
+	return zero, false
 }
 
 // Set sets m[key] = val.
 func (m *Map[K, V]) Set(key K, val V) {
-	parent, pos := m.lookup(key)
-	set(parent, pos, key, val, &m.root)
+	set(m, key, val)
 }
 
 // Set sets m[key] = val.
 func (m *MapFunc[K, V]) Set(key K, val V) {
-	parent, pos := m.lookup(key)
-	set(parent, pos, key, val, &m.root)
+	set(m, key, val)
 }
 
-// set sets m[key] = val, where parent, pos are from m.lookup(k) and root is &m.root.
-func set[K, V any](parent *node[K, V], pos **node[K, V], key K, val V, root **node[K, V]) {
-	x := *pos
-	if x != nil {
+func set[K, V any](m omap[K, V], key K, val V) {
+	pos, parent := m.find(key)
+	if x := *pos; x != nil {
 		x.val = val
 		return
 	}
-	x = &node[K, V]{key: key, val: val, pri: rand.Uint64(), parent: parent}
+	x := &node[K, V]{key: key, val: val, pri: rand.Uint64() | 1, parent: parent}
 	*pos = x
+	rotateUp(m, x)
+}
 
+// rotateUp rotates x upward in the tree to correct any priority inversions.
+func rotateUp[K, V any](m omap[K, V], x *node[K, V]) {
 	// Rotate up into tree according to priority.
 	for x.parent != nil && x.parent.pri > x.pri {
-		if x.parent.prev == x {
-			x.parent.rotateRight(root)
-		} else if x.parent.next == x {
-			x.parent.rotateLeft(root)
+		if x.parent.left == x {
+			rotateRight(m, x.parent)
 		} else {
-			panic("corrupt treap")
+			rotateLeft(m, x.parent)
 		}
 	}
 }
 
 // Delete deletes m[key].
 func (m *Map[K, V]) Delete(key K) {
-	_, pos := m.lookup(key)
-	_delete(pos, &m.root)
+	_delete(m, key)
 }
 
 // Delete deletes m[key].
 func (m *MapFunc[K, V]) Delete(key K) {
-	_, pos := m.lookup(key)
-	_delete(pos, &m.root)
+	_delete(m, key)
 }
 
-// _delete (not delete, to avoid shadowing builtin)
-// deletes m[key], where pos is from m.lookup(key) and root is &m.root.
-func _delete[K, V any](pos, root **node[K, V]) {
+func _delete[K, V any](m omap[K, V], key K) {
+	pos, _ := m.find(key)
 	x := *pos
 	if x == nil {
 		return
 	}
 
 	// Rotate x down to be leaf of tree for removal, respecting priorities.
-	for x.next != nil || x.prev != nil {
-		if x.next == nil || x.prev != nil && x.prev.pri < x.next.pri {
-			x.rotateRight(root)
+	for x.right != nil || x.left != nil {
+		if x.right == nil || x.left != nil && x.left.pri < x.right.pri {
+			rotateRight(m, x)
 		} else {
-			x.rotateLeft(root)
+			rotateLeft(m, x)
 		}
 	}
 
 	// Remove x, now a leaf.
-	if p := x.parent; p != nil {
-		if p.prev == x {
-			p.prev = nil
-		} else {
-			p.next = nil
-		}
-	} else {
-		*root = nil
+	switch p := x.parent; {
+	case p == nil:
+		*m.root() = nil
+	case p.left == x:
+		p.left = nil
+	default:
+		p.right = nil
+	}
+	x.pri = 0 // mark deleted
+}
+
+// DeleteRange deletes m[k] for all keys k satisfying lo ≤ k ≤ hi.
+func (m *Map[K, V]) DeleteRange(lo, hi K) {
+	if lo > hi {
+		return
+	}
+	deleteRange(m, lo, hi)
+}
+
+// DeleteRange deletes m[k] for all keys k satisfying lo ≤ k ≤ hi.
+func (m *MapFunc[K, V]) DeleteRange(lo, hi K) {
+	if m.cmp(lo, hi) > 0 {
+		return
+	}
+	deleteRange(m, lo, hi)
+}
+
+func deleteRange[K, V any](m omap[K, V], lo, hi K) {
+	after := split(m, hi)
+	middle := split(m, lo)
+	markDeleted(middle)
+	if after != nil {
+		pos, parent := m.find(lo)
+		*pos = after
+		after.parent = parent
+		rotateUp(m, after)
 	}
 }
 
-// Get returns the value of m[key] and reports whether it exists.
-func (m *Map[K, V]) Get(key K) (V, bool) {
-	return get(m.lookup(key))
-}
-
-// Get returns the value of m[key] and reports whether it exists.
-func (m *MapFunc[K, V]) Get(key K) (V, bool) {
-	return get(m.lookup(key))
-}
-
-func get[K, V any](_ *node[K, V], pos **node[K, V]) (V, bool) {
+func split[K, V any](m omap[K, V], key K) (before *node[K, V]) {
+	pos, parent := m.find(key)
 	if *pos == nil {
-		var zero V
-		return zero, false
+		*pos = &node[K, V]{parent: parent}
 	}
-	return (*pos).val, true
+	x := *pos
+	x.pri = 0
+	rotateUp(m, x)
+
+	*m.root() = x.left
+	if x.left != nil {
+		x.left.parent = nil
+	}
+	return x.right
+}
+
+func markDeleted[K, V any](x *node[K, V]) {
+	if x == nil {
+		return
+	}
+	x.pri = 0
+	markDeleted(x.left)
+	markDeleted(x.right)
 }
 
 // All returns an iterator over the map m.
 // If m is modified during the iteration, some keys may not be visited.
 // No keys will be visited multiple times.
 func (m *Map[K, V]) All() iter.Seq2[K, V] {
-	return all(&m.root)
+	return all(m)
 }
 
 // All returns an iterator over the map m.
 // If m is modified during the iteration, some keys may not be visited.
 // No keys will be visited multiple times.
 func (m *MapFunc[K, V]) All() iter.Seq2[K, V] {
-	return all(&m.root)
+	return all(m)
 }
 
 // all returns an iterator over the map m, where *root is the root.
-func all[K, V any](root **node[K, V]) iter.Seq2[K, V] {
+func all[K, V any](m omap[K, V]) iter.Seq2[K, V] {
 	return func(yield func(K, V) bool) {
-		x := *root
+		x := *m.root()
 		if x != nil {
-			for x.prev != nil {
-				x = x.prev
+			for x.left != nil {
+				x = x.left
 			}
 		}
 		for x != nil && yield(x.key, x.val) {
-			x = x.step()
+			x = x.next(m)
 		}
 	}
 }
@@ -213,16 +283,9 @@ func all[K, V any](root **node[K, V]) iter.Seq2[K, V] {
 // No keys will be visited multiple times.
 func (m *Map[K, V]) Scan(lo, hi K) iter.Seq2[K, V] {
 	return func(yield func(K, V) bool) {
-		parent, pos := m.lookup(lo)
-		x := *pos
-		if x == nil {
-			x = parent
-			if x.key < lo {
-				x = x.step()
-			}
-		}
+		x, _ := findGE(m, lo)
 		for x != nil && x.key <= hi && yield(x.key, x.val) {
-			x = x.step()
+			x = x.next(m)
 		}
 	}
 }
@@ -234,88 +297,108 @@ func (m *Map[K, V]) Scan(lo, hi K) iter.Seq2[K, V] {
 // No keys will be visited multiple times.
 func (m *MapFunc[K, V]) Scan(lo, hi K) iter.Seq2[K, V] {
 	return func(yield func(K, V) bool) {
-		parent, pos := m.lookup(lo)
-		x := *pos
-		if x == nil {
-			x = parent
-			if m.cmp(x.key, lo) < 0 {
-				x = x.step()
-			}
-		}
+		x, _ := findGE(m, lo)
 		for x != nil && m.cmp(x.key, hi) <= 0 && yield(x.key, x.val) {
-			x = x.step()
+			x = x.next(m)
 		}
 	}
 }
 
-// step returns the successor node of x in the treap.
-func (x *node[K, V]) step() *node[K, V] {
-	if x == nil {
-		panic("treap step nil")
+// next returns the successor node of x in the treap,
+// even if x has been removed from the treap.
+// x must not be nil.
+func (x *node[K, V]) next(m omap[K, V]) *node[K, V] {
+	if x.pri == 0 {
+		// x has been deleted.
+		// Find where x.key would be in the current tree.
+		var eq bool
+		x, eq = findGE(m, x.key)
+		if !eq {
+			// The new x is already greater than the old x.key.
+			return x
+		}
 	}
-	if x.next == nil {
-		for x.parent != nil && x.parent.next == x {
+
+	if x.right == nil {
+		for x.parent != nil && x.parent.right == x {
 			x = x.parent
 		}
 		return x.parent
 	}
-	x = x.next
-	for x.prev != nil {
-		x = x.prev
+	x = x.right
+	for x.left != nil {
+		x = x.left
 	}
 	return x
 }
 
+// findGE finds the node x in m with the least key k such that k ≥ key.
+func findGE[K, V any](m omap[K, V], key K) (x *node[K, V], eq bool) {
+	pos, parent := m.find(key)
+	if *pos != nil {
+		return *pos, true
+	}
+	if parent == nil {
+		return nil, false
+	}
+	if pos == &parent.left {
+		return parent, false
+	}
+	return parent.next(m), false
+}
+
 // rotateLeft rotates the subtree rooted at node x.
 // turning (x a (y b c)) into (y (x a b) c).
-func (x *node[K, V]) rotateLeft(root **node[K, V]) {
+func rotateLeft[K, V any](m omap[K, V], x *node[K, V]) {
 	// p -> (x a (y b c))
 	p := x.parent
-	y := x.next
-	b := y.prev
+	y := x.right
+	b := y.left
 
-	y.prev = x
+	y.left = x
 	x.parent = y
-	x.next = b
+	x.right = b
 	if b != nil {
 		b.parent = x
 	}
 
 	y.parent = p
 	if p == nil {
-		*root = y
-	} else if p.prev == x {
-		p.prev = y
-	} else if p.next == x {
-		p.next = y
+		*m.root() = y
+	} else if p.left == x {
+		p.left = y
+	} else if p.right == x {
+		p.right = y
 	} else {
+		// unreachable
 		panic("corrupt treap")
 	}
 }
 
 // rotateRight rotates the subtree rooted at node y.
 // turning (y (x a b) c) into (x a (y b c)).
-func (y *node[K, V]) rotateRight(root **node[K, V]) {
+func rotateRight[K, V any](m omap[K, V], y *node[K, V]) {
 	// p -> (y (x a b) c)
 	p := y.parent
-	x := y.prev
-	b := x.next
+	x := y.left
+	b := x.right
 
-	x.next = y
+	x.right = y
 	y.parent = x
-	y.prev = b
+	y.left = b
 	if b != nil {
 		b.parent = y
 	}
 
 	x.parent = p
 	if p == nil {
-		*root = x
-	} else if p.prev == y {
-		p.prev = x
-	} else if p.next == y {
-		p.next = x
+		*m.root() = x
+	} else if p.left == y {
+		p.left = x
+	} else if p.right == y {
+		p.right = x
 	} else {
+		// unreachable
 		panic("corrupt treap")
 	}
 }
--- omap_test.go
+++ omap_test.go
@@ -8,102 +8,282 @@ package omap
 
 import (
 	"bytes"
+	"cmp"
 	"fmt"
+	"iter"
 	"math/rand/v2"
 	"slices"
 	"testing"
 )
 
-func Test(t *testing.T) {
-	for range 10 {
-		const N = 10
-		var tr Map[int, int]
-		perm := rand.Perm(N)
-		inv := make([]int, N)
-		for i, x := range perm {
-			tr.Set(x, i)
-			inv[x] = i
-		}
-
-		if false { // print treap
-			var buf bytes.Buffer
-			var walk func(*node[int, int])
-			walk = func(x *node[int, int]) {
-				if x == nil {
-					fmt.Fprintf(&buf, "nil")
-					return
-				}
-				fmt.Fprintf(&buf, "(%d ", x.key)
-				walk(x.prev)
-				fmt.Fprintf(&buf, " ")
-				walk(x.next)
-				fmt.Fprintf(&buf, ")")
-			}
-			walk(tr.root)
-			t.Logf("treap: %s", buf.String())
-		}
-
-		for i, x := range perm {
-			v, ok := tr.Get(x)
-			if v != i || !ok {
-				t.Errorf("Get(%d) = %d, %v, want %d, true", x, v, ok, i)
-			}
-		}
-
-		var all []int
-		for k, v := range tr.All() {
-			if v != inv[k] {
-				t.Errorf("All() returned %d, %d want %d, %d", k, v, k, inv[k])
-			}
-			all = append(all, k)
-			if len(all) > N+5 {
-				break
-			}
-		}
-		if !match(all, 0, N-1) {
-			t.Errorf("All() = %v, want 0..%d", all, N-1)
-		}
-		for lo := -1; lo <= N; lo++ {
-			for hi := lo; hi <= N; hi++ {
-				var scan []int
-				for k, v := range tr.Scan(lo, hi) {
-					if v != inv[k] {
-						t.Errorf("Scan() returned %d, %d want %d, %d", k, v, k, inv[k])
-					}
-					scan = append(scan, k)
-					if len(scan) > N+5 {
-						break
-					}
-				}
-				if !match(scan, max(lo, 0), min(hi, N-1)) {
-					t.Errorf("Scan(%d, %d) = %v, want %d..%d", lo, hi, scan, lo, hi)
-				}
-			}
-		}
-
-		for i, x := range perm {
-			tr.Delete(x)
-			var list []int
-			for k, _ := range tr.All() {
-				list = append(list, k)
-			}
-			want := slices.Clone(perm[i+1:])
-			slices.Sort(want)
-			if !slices.Equal(list, want) {
-				t.Errorf("after Delete [%v], All() = %v, want %v", perm[:i+1], list, want)
-			}
-		}
-	}
+type Interface[K, V any] interface {
+	All() iter.Seq2[K, V]
+	Delete(key K)
+	DeleteRange(lo, hi K)
+	Get(key K) (V, bool)
+	Scan(lo, hi K) iter.Seq2[K, V]
+	Set(key K, val V)
+	root() **node[K, V]
 }
 
-func match(xs []int, lo, hi int) bool {
-	if len(xs) != hi+1-lo {
-		return false
+func permute(m Interface[int, int], n int) (perm, slice []int) {
+	perm = rand.Perm(n)
+	slice = make([]int, 2*n+1)
+	for i, x := range perm {
+		m.Set(2*x+1, i+1)
+		slice[2*x+1] = i + 1
 	}
-	for i, x := range xs {
-		if x != lo+i {
-			return false
+	// Overwrite-Set half the entries.
+	for i, x := range perm[:len(perm)/2] {
+		m.Set(2*x+1, i+100)
+		slice[2*x+1] = i + 100
+	}
+	return perm, slice
+}
+
+func dump(m Interface[int, int]) string {
+	var buf bytes.Buffer
+	var walk func(*node[int, int])
+	walk = func(x *node[int, int]) {
+		if x == nil {
+			fmt.Fprintf(&buf, "nil")
+			return
 		}
+		fmt.Fprintf(&buf, "(%d ", x.key)
+		walk(x.left)
+		fmt.Fprintf(&buf, " ")
+		walk(x.right)
+		fmt.Fprintf(&buf, ")")
 	}
-	return true
+	walk(*m.root())
+	return buf.String()
+}
+
+func test(t *testing.T, f func(*testing.T, func() Interface[int, int])) {
+	t.Run("Map", func(t *testing.T) {
+		f(t, func() Interface[int, int] { return new(Map[int, int]) })
+	})
+	t.Run("MapFunc", func(t *testing.T) {
+		f(t, func() Interface[int, int] { return NewMapFunc[int, int](cmp.Compare) })
+	})
+}
+
+func TestGet(t *testing.T) {
+	test(t, func(t *testing.T, newMap func() Interface[int, int]) {
+		for N := range 11 {
+			m := newMap()
+			_, slice := permute(m, N)
+			for k, want := range slice {
+				v, ok := m.Get(k)
+				if v != want || ok != (want > 0) {
+					t.Fatalf("Get(%d) = %d, %v, want %d, %v\nM: %v", k, v, ok, want, want > 0, dump(m))
+				}
+			}
+		}
+	})
+}
+
+func TestAll(t *testing.T) {
+	test(t, func(t *testing.T, newMap func() Interface[int, int]) {
+		for N := range 11 {
+			m := newMap()
+			_, slice := permute(m, N)
+			var have []int
+			for k, v := range m.All() {
+				if v != slice[k] {
+					t.Errorf("All() returned %d, %d want %d, %d", k, v, k, slice[k])
+				}
+				have = append(have, k)
+				if len(have) > N+5 { // too many; looping?
+					break
+				}
+			}
+			var want []int
+			for k, v := range slice {
+				if v != 0 {
+					want = append(want, k)
+				}
+			}
+			if !slices.Equal(have, want) {
+				t.Errorf("All() = %v, want %v", have, want)
+			}
+		}
+	})
+}
+
+func TestScan(t *testing.T) {
+	test(t, func(t *testing.T, newMap func() Interface[int, int]) {
+		for N := range 11 {
+			m := newMap()
+			_, slice := permute(m, N)
+			for hi := range slice {
+				for lo := range hi + 1 {
+					var have []int
+					for k, v := range m.Scan(lo, hi) {
+						if v != slice[k] {
+							t.Errorf("All() returned %d, %d want %d, %d", k, v, k, slice[k])
+						}
+						have = append(have, k)
+						if len(have) > N+5 { // too many; looping?
+							break
+						}
+					}
+					var want []int
+					for k, v := range slice {
+						if v != 0 && lo <= k && k <= hi {
+							want = append(want, k)
+						}
+					}
+					if !slices.Equal(have, want) {
+						t.Errorf("All() = %v, want %v", have, want)
+					}
+				}
+			}
+		}
+	})
+}
+
+func TestDelete(t *testing.T) {
+	test(t, func(t *testing.T, newMap func() Interface[int, int]) {
+		for N := range 11 {
+			m := newMap()
+			_, slice := permute(m, N)
+			for _, x := range rand.Perm(len(slice)) {
+				m.Delete(x)
+				slice[x] = 0
+				var have []int
+				for k, _ := range m.All() {
+					have = append(have, k)
+				}
+				var want []int
+				for x, v := range slice {
+					if v != 0 {
+						want = append(want, x)
+					}
+				}
+				slices.Sort(want)
+				if !slices.Equal(have, want) {
+					t.Errorf("after Delete(%v), All() = %v, want %v", x, have, want)
+				}
+			}
+		}
+	})
+}
+
+func TestDeleteRange(t *testing.T) {
+	test(t, func(t *testing.T, newMap func() Interface[int, int]) {
+		for N := range 11 {
+			for hi := range 2*N + 1 {
+				for lo := range hi + 1 {
+					m := newMap()
+					_, slice := permute(m, N)
+					if lo < hi {
+						m.DeleteRange(hi, lo) // want no-op
+					}
+					m.DeleteRange(lo, hi)
+					var have []int
+					for k, _ := range m.All() {
+						have = append(have, k)
+					}
+					var want []int
+					for k, v := range slice {
+						if v != 0 && (k < lo || hi < k) {
+							want = append(want, k)
+						}
+					}
+					if !slices.Equal(have, want) {
+						t.Errorf("after DeleteRange(%d, %d), All() = %v, want %v", lo, hi, have, want)
+					}
+				}
+			}
+		}
+	})
+}
+
+func TestScanDelete(t *testing.T) {
+	test(t, func(t *testing.T, newMap func() Interface[int, int]) {
+		for _, mode := range []string{"prev", "current", "next"} {
+			for N := range 8 {
+				for target := 1; target <= 2*N-1; target += 2 {
+					m := newMap()
+					_, slice := permute(m, N)
+					var have []int
+					var deleted int
+					for k, _ := range m.All() {
+						if k == target {
+							switch mode {
+							case "prev":
+								deleted = k - 2
+							case "current":
+								deleted = k
+							case "next":
+								deleted = k + 2
+								if k+2 < len(slice) {
+									slice[k+2] = 0
+								}
+							}
+							m.Delete(deleted)
+						}
+						have = append(have, k)
+					}
+					var want []int
+					for k, v := range slice {
+						if v != 0 {
+							want = append(want, k)
+						}
+					}
+					if !slices.Equal(have, want) {
+						t.Errorf("All() with Delete(%d) at %d = %v, want %v", deleted, target, have, want)
+					}
+				}
+			}
+		}
+	})
+}
+
+func TestScanDeleteRange(t *testing.T) {
+	test(t, func(t *testing.T, newMap func() Interface[int, int]) {
+		for _, mode := range []string{"prev", "current", "next"} {
+			for N := range 8 {
+				for target := 1; target <= 2*N-1; target += 2 {
+					m := newMap()
+					_, slice := permute(m, N)
+					var have []int
+					var deleteLo, deleteHi int
+					for k, _ := range m.All() {
+						if k == target {
+							switch mode {
+							case "prev":
+								deleteLo, deleteHi = k-5, k-1
+							case "current":
+								deleteLo, deleteHi = k-2, k+2
+								if k+2 < len(slice) {
+									slice[k+2] = 0
+								}
+							case "next":
+								deleteLo, deleteHi = k+1, k+5
+								if k+2 < len(slice) {
+									slice[k+2] = 0
+								}
+								if k+4 < len(slice) {
+									slice[k+4] = 0
+								}
+							}
+							m.DeleteRange(deleteLo, deleteHi)
+						}
+						have = append(have, k)
+					}
+					var want []int
+					for k, v := range slice {
+						if v != 0 {
+							want = append(want, k)
+						}
+					}
+					if !slices.Equal(have, want) {
+						t.Errorf("All() with DeleteRange(%d, %d) at %d = %v, want %v", deleteLo, deleteHi, target, have, want)
+					}
+				}
+			}
+		}
+	})
 }
